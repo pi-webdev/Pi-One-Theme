e31c92c30adb48c47a34a5ec2ff463f9770b7cff
Deniss <61137506+pi-webdev@users.noreply.github.com>
2021-04-06 10:20:17 +0300

    Change class from col to col-12

--- footer-widget.php
+++ footer-widget.php
@@ -7,13 +7,13 @@ if ( is_active_sidebar( 'footer-1' ) || is_active_sidebar( 'footer-2' ) || is_ac
            <div class="container">
                 <div class="row">
                     <?php if ( is_active_sidebar( 'footer-1' )) : ?>
-                        <div class="col col-md-4 p-0"><div class="card mt-4 mb-4"><div class="card-body "><?php dynamic_sidebar( 'footer-1' ); ?></div></div></div>
+                        <div class="col-12 col-md-4 p-0"><div class="card mt-4 mb-4"><div class="card-body "><?php dynamic_sidebar( 'footer-1' ); ?></div></div></div>
                     <?php endif; ?>
                     <?php if ( is_active_sidebar( 'footer-2' )) : ?>
-                        <div class="col col-md-4 p-0"><div class="card mt-4 mb-4 ml-4 mr-4"><div class="card-body"><?php dynamic_sidebar( 'footer-2' ); ?></div></div></div>
+                        <div class="col-12 col-md-4 p-0"><div class="card mt-4 mb-4 ml-4 mr-4"><div class="card-body"><?php dynamic_sidebar( 'footer-2' ); ?></div></div></div>
                     <?php endif; ?>
                     <?php if ( is_active_sidebar( 'footer-3' )) : ?>
-                        <div class="col col-md-4 p-0"><div class="card mt-4 mb-4"><div class="card-body "><?php dynamic_sidebar( 'footer-3' ); ?></div></div></div>
+                        <div class="col-12 col-md-4 p-0"><div class="card mt-4 mb-4"><div class="card-body "><?php dynamic_sidebar( 'footer-3' ); ?></div></div></div>
                     <?php endif; ?>
                 </div>
             </div>        
