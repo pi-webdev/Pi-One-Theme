56ffe61a540d03f4f68b85841621c21df70683b4
Deniss <61137506+pi-webdev@users.noreply.github.com>
2021-05-05 16:44:45 +0300

    remove margins

--- sidebar-header.php
+++ sidebar-header.php
@@ -6,13 +6,13 @@ if ( is_active_sidebar( 'contact-nav' ) || is_active_sidebar( 'social-nav' ) || 
        <div class="container-fluid">
             <div class="row">
                 <?php if ( is_active_sidebar( 'contact-nav' )) : ?>
-                    <div class="col col-md-4 top-nav-left my-auto mr-auto"><?php dynamic_sidebar( 'contact-nav' ); ?></div>
+                    <div class="col-12 col-md-4 top-nav-left my-auto"><?php dynamic_sidebar( 'contact-nav' ); ?></div>
                 <?php endif; ?>
                 <?php if ( is_active_sidebar( 'social-nav' )) : ?>
-                    <div class="col col-md-4 top-nav-middle my-auto mx-auto"><?php dynamic_sidebar( 'social-nav' ); ?></div>
+                    <div class="col-12 col-md-4 top-nav-middle my-auto"><?php dynamic_sidebar( 'social-nav' ); ?></div>
                 <?php endif; ?>
                 <?php if ( is_active_sidebar( 'cart-nav' )) : ?>
-                    <div class="col col-md-4 top-nav-right my-auto ml-auto"><?php dynamic_sidebar( 'cart-nav' ); ?></div>
+                    <div class="col-12 col-md-4 top-nav-right my-auto"><?php dynamic_sidebar( 'cart-nav' ); ?></div>
                 <?php endif; ?>
             </div>
         </div>        
