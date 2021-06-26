1be33d148fd0a0f69097c22722abbbca44a84fb2
Deniss <61137506+pi-webdev@users.noreply.github.com>
2021-06-26 23:48:15 +0300

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -11,7 +11,7 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 	return;
 }
 ?>
-<aside id="secondary" class="site-content col-sm-12 col-lg-4" role="complementary">
+<aside id="secondary" class="site-content col-sm-12 col-lg-4">
 <div class="card mb-3">
   <div class="card-body">
   <?php dynamic_sidebar( 'sidebar-1' ); ?>
