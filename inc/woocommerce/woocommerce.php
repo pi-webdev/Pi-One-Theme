52168749173cacadb96e343ef824890a04bfb099
Deniss <61137506+pi-webdev@users.noreply.github.com>
2021-06-28 22:23:59 +0300

    File update

--- inc/woocommerce/woocommerce.php
+++ inc/woocommerce/woocommerce.php
@@ -1,19 +1,107 @@
 <?php
 
-add_action( 'after_setup_theme', 'woocommerce_support' );
-function woocommerce_support() {
-add_theme_support( 'woocommerce' );
-    add_theme_support( 'wc-product-gallery-zoom' );
-    add_theme_support( 'wc-product-gallery-lightbox' );
-    add_theme_support( 'wc-product-gallery-slider' );
+
+
+
+
+// If plugin - 'WooCommerce' not exist then return.
+if ( ! class_exists( 'WooCommerce' ) ) {
+	return;
 }
 
+/**
+ * Pi-one WooCommerce Compatibility
+ */
+add_action( 'after_setup_theme', 'woocommerce_support' );
+function woocommerce_support() {
+    add_theme_support( 'woocommerce', array(
+        'thumbnail_image_width' => 150,
+        'single_image_width'    => 300,
+
+        'product_grid'          => array(
+            'default_rows'    => 3,
+            'min_rows'        => 1,
+            'max_rows'        => 8,
+            'default_columns' => 4,
+            'min_columns'     => 1,
+            'max_columns'     => 5,
+        ),
+    ) );
+add_theme_support( 'wc-product-gallery-zoom' );
+add_theme_support( 'wc-product-gallery-lightbox' );
+add_theme_support( 'wc-product-gallery-slider' );
+
+}
 
 /*********************************************************************************************/
 /** WooCommerce - Modify each individual input type $args defaults /**
 /*********************************************************************************************/
 
 add_filter('woocommerce_form_field_args','wc_form_field_args',10,3);
+
+// Add the cart link to menu
+function pione_add_menu_cart_item_to_menus( $items, $args ) {
+
+	// Make sure your change 'pione_main' to your Menu location !!!!
+	if ( $args->theme_location === 'primary' ) {
+
+		$css_class = 'menu-item menu-item-type-cart menu-item-type-woocommerce-cart';
+
+		if ( is_cart() ) {
+			$css_class .= 'current-menu-item';
+		}
+
+		$items .= '<li class="' . esc_attr( $css_class ) . '">';
+
+			$items .= pione_menu_cart_item();
+
+		$items .= '</li>';
+
+	}
+
+	return $items;
+
+}
+add_filter( 'wp_nav_menu_items', 'pione_add_menu_cart_item_to_menus', 10, 2 );
+
+// Function returns the main menu cart link
+function pione_menu_cart_item() {
+
+	$output = '';
+
+	$cart_count = WC()->cart->cart_contents_count;
+
+	$css_class = 'nav-link ';
+
+	if ( $cart_count ) {
+		$url  = WC()->cart->get_cart_url();
+	} else {
+		$url  = wc_get_page_permalink( 'shop' );
+	}
+
+	$html = $cart_extra = WC()->cart->get_cart_total();
+	$html = str_replace( 'amount', '', $html );
+
+	$output .= '<a href="'. esc_url( $url ) .'" class="' . esc_attr( $css_class ) . '">';
+
+		$output .= '<i class="bi bi-bag"></i>';
+
+		$output .= wp_kses_post( $html );
+
+	$output .= '</a>';
+
+	return $output;
+}
+
+
+// Update cart link with AJAX
+function pione_main_menu_cart_link_fragments( $fragments ) {
+	$fragments['.pione-menu-cart-total'] = pione_menu_cart_item();
+	return $fragments;
+}
+add_filter( 'add_to_cart_fragments', 'pione_main_menu_cart_link_fragments' );
+//endwoocommerce cart
+
 
 function wc_form_field_args( $args, $key, $value = null ) {
 
